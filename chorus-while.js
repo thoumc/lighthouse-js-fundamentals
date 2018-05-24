535b5c6e1240c3c13db9f94ac3441351684d9b3d
thoumc <thoumc@hotmail.com>
2018-05-24 11:33:41 +0000

    Add looping exercises

--- chorus-while.js
+++ chorus-while.js
@@ -1,5 +1,13 @@
 var chorus = "Let's dance!";
 var repeat = 0;
+
+
+while (repeat < 10) {
+  console.log(chorus);
+  repeat++;
+}
+console.log("Until the sun comes up!");
+
 while (repeat < 10) {
   if (repeat === 5) {
     console.log("*change key*");
---
+++ ingredients.js
@@ -0,0 +1,19 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+//while loop
+var list = 0
+while (list < ingredients.length){
+  console.log(ingredients[list]);
+  list++
+}
+
+// for loop
+
+for (var i = 0; i < ingredients.length; i++){
+  console.log(ingredients [i]);
+}
+
+//backwards loop
+for (var i = ingredients.length; i >= 0; i--){
+  console.log(ingredients [i]);
+}
