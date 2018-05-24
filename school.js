7881d6c38bb3b142e066ae5be7b4303e920731f9
thoumc <thoumc@hotmail.com>
2018-05-24 11:08:06 +0000

    Adds lopping exercises

---
+++ school.js
@@ -0,0 +1,17 @@
+function whichSchool (age){
+  if (age < 13) {
+    return "Elementary School";
+  } else if (12 < age, age < 19){
+    return "Secondary School";
+  }
+  else if (age > 18) {
+    return "Lighthouse Labs";
+  }
+}
+
+console.log("I am 35. Which school should I go to?");
+console.log(whichSchool(35));
+console.log("I am 8. Which school should I go to?");
+console.log(whichSchool(8));
+console.log("I am 14. Which school should I go to?");
+console.log(whichSchool(14));
